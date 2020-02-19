5005b07729302686debb742af2571f6f4d4259bb
Duncan Doyle <Duncan.Doyle@gmail.com>
2020-02-19 15:39:36 +0100

    Fully implemented step3 of lab2.

---
+++ src/main/java/org/optaplanner/examples/cloudbalancing/optional/score/CloudBalancingConstraintProvider.java
@@ -0,0 +1,67 @@
+package org.optaplanner.examples.cloudbalancing.optional.score;
+
+import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
+import org.optaplanner.core.api.score.stream.Constraint;
+import org.optaplanner.core.api.score.stream.ConstraintFactory;
+import org.optaplanner.core.api.score.stream.ConstraintProvider;
+import org.optaplanner.examples.cloudbalancing.domain.CloudComputer;
+import org.optaplanner.examples.cloudbalancing.domain.CloudProcess;
+
+import static org.optaplanner.core.api.score.stream.ConstraintCollectors.sum;
+
+public class CloudBalancingConstraintProvider implements ConstraintProvider {
+
+    @Override
+    public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
+        return new Constraint[]{
+                requiredCpuPowerTotal(constraintFactory),
+                requiredMemoryTotal(constraintFactory),
+                requiredNetworkBandwidthTotal(constraintFactory),
+                computerCost(constraintFactory)
+        };
+    }
+
+    // ************************************************************************
+    // Hard constraints
+    // ************************************************************************
+
+    private Constraint requiredCpuPowerTotal(ConstraintFactory constraintFactory) {
+        return constraintFactory.from(CloudProcess.class)
+                .groupBy(CloudProcess::getComputer, sum(CloudProcess::getRequiredCpuPower))
+                .filter((computer, requiredCpuPower) -> requiredCpuPower > computer.getCpuPower())
+                .penalize("requiredCpuPowerTotal",
+                        HardSoftScore.ONE_HARD,
+                        (computer, requiredCpuPower) -> requiredCpuPower - computer.getCpuPower());
+    }
+
+    private Constraint requiredMemoryTotal(ConstraintFactory constraintFactory) {
+        return constraintFactory.from(CloudProcess.class)
+                .groupBy(CloudProcess::getComputer, sum(CloudProcess::getRequiredMemory))
+                .filter((computer, requiredMemory) -> requiredMemory > computer.getMemory())
+                .penalize("requiredMemoryTotal",
+                        HardSoftScore.ONE_HARD,
+                        (computer, requiredMemory) -> requiredMemory - computer.getMemory());
+    }
+
+    private Constraint requiredNetworkBandwidthTotal(ConstraintFactory constraintFactory) {
+        return constraintFactory.from(CloudProcess.class)
+                .groupBy(CloudProcess::getComputer, sum(CloudProcess::getRequiredNetworkBandwidth))
+                .filter((computer, requiredNetworkBandwidth) -> requiredNetworkBandwidth > computer.getNetworkBandwidth())
+                .penalize("requiredNetworkBandwidthTotal",
+                        HardSoftScore.ONE_HARD,
+                        (computer, requiredNetworkBandwidth) -> requiredNetworkBandwidth - computer.getNetworkBandwidth());
+    }
+
+    // ************************************************************************
+    // Soft constraints
+    // ************************************************************************
+
+    private Constraint computerCost(ConstraintFactory constraintFactory) {
+        return constraintFactory.from(CloudProcess.class)
+                .groupBy(CloudProcess::getComputer)
+                .penalize("computerCost",
+                        HardSoftScore.ONE_SOFT,
+                        CloudComputer::getCost);
+    }
+
+}
